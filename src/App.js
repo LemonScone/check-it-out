321c02784bcdd5302ef1acd56fba5e73e4d40e0e
the1020 <yenn2322@gmail.com>
2021-09-23 17:40:12 +0900

    💄 add material-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Toolbar from './components/Toolbar'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">check it out</header>
+      <Toolbar name="check-it-out"></Toolbar>
     </div>
   )
 }
