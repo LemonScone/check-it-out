12a210f1d91ee3a50cbddf2498cc0e40965fe23f
padosum <makalu171@gmail.com>
2021-10-19 17:33:27 +0900

    🚧 add table #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Toolbar from './components/Toolbar';
+import ProgramTable from './components/ProgramTable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <Toolbar name="check-it-out"></Toolbar>
+      <ProgramTable></ProgramTable>
     </div>
   );
 }
