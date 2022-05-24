528527d9957dfbb693ea8678a91087dc180a726e
the1020 <yenn2322@gmail.com>
2022-05-24 17:47:46 +0900

    :sparkles: electron-store로 데이터 관리, 활성시간 포맷 변경

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,13 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+const { format, intervalToDuration, formatDuration } = require('date-fns');
 const formatDistanceStrict = require('date-fns/formatDistanceStrict');
+const Store = require('electron-store');
+const activeWin = require('active-win');
 const { storeAppIcon } = require('./utils/icon');
-//require('devtron').install();
 
-const activeWindows = [];
-let currentActiveWin;
+const store = new Store();
 
 app.whenReady().then(() => {
   let win = new BrowserWindow({
@@ -27,8 +28,11 @@ app.whenReady().then(() => {
   }
 
   ipcMain.on('ACTIVE_WINDOW', async (event, payload) => {
-    const activeWin = require('active-win');
     let window = await activeWin({ screenRecordingPermission: true });
+
+    //* store에는 "trackWindows"와 "currentActiveWin"이 저장되어 있음
+    const trackWindows = store.get('trackWindows') || [];
+    const currentActiveWin = store.get('currentActiveWin') || null;
 
     const {
       id,
@@ -50,23 +54,27 @@ app.whenReady().then(() => {
     };
 
     //* 첫 할당이거나 활성 프로그램이 변했으면 재할당
-    //TODO 아무 프로그램도 활성화되어 있지 않을 때 (=바탕화면에 포커스가 잡혀있는 경우)
     if (!currentActiveWin || currentActiveWin.id !== processedWindow.id) {
-      //* 날짜 객체 생성해서 차이 구하기
-      const startedDate = currentActiveWin ? currentActiveWin.startDate : processedWindow.startDate;
-      const finishedDate = processedWindow.startDate;
-
-      const distance = formatDistanceStrict(startedDate, finishedDate, {
-        unit: 'second',
-      });
-
+      //* 활성 프로그램이 바꼈으면 이전 프로그램 정보에 finishedDate 정보 추가해서 store에 저장
       if (currentActiveWin) {
-        [currentActiveWin.finishedDate, currentActiveWin.distance] = [finishedDate, distance];
-      }
+        //* 날짜 객체 생성해서 차이 구하기
+        const { startDate } = currentActiveWin;
+        const { startDate: finishedDate } = processedWindow;
+        const distance = getDistanceDate(startDate, finishedDate);
 
-      currentActiveWin = processedWindow;
-      activeWindows.push(processedWindow);
+        //* 이전의 active-win 객체에 finishedDate와 distance 정보 업데이트
+        trackWindows.forEach((w) => {
+          if (w.id === currentActiveWin.id && w.startDate === currentActiveWin.startDate) {
+            [w.finishedDate, w.distance] = [finishedDate, distance];
+          }
+        });
+      }
+      trackWindows.push(processedWindow);
+      store.set('trackWindows', trackWindows);
+      store.set('currentActiveWin', processedWindow);
     }
+
+    const activeWindows = findTrackWindowByDate(new Date());
 
     event.reply('REPLY_ACTIVE_WINDOW', { processedWindow, activeWindows });
   });
@@ -85,3 +93,47 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   app.quit();
 });
+
+const findTrackWindowByDate = (comparedDate) => {
+  comparedDate = typeof date === 'string' ? new Date(comparedDate) : comparedDate;
+  const filtered = store
+    .get('trackWindows')
+    .filter(
+      (w) => format(new Date(w.startDate), 'yyyy-MM-dd') === format(comparedDate, 'yyyy-MM-dd'),
+    );
+
+  filtered.forEach(
+    (w) =>
+      ([w.startDate, w.finishedDate] = [
+        new Date(w.startDate),
+        w.finishedDate ? new Date(w.finishedDate) : null,
+      ]),
+  );
+
+  return filtered;
+};
+
+const getDistanceDate = (baseDate, comparedDate) => {
+  const formatDistanceLocale = {
+    xSeconds: (count) => `${count}sec`,
+    xMinutes: (count) => `${count}min`,
+    xHours: (count) => `${count}hour`,
+  };
+
+  const shortLocale = {
+    formatDistance: (token, count) => formatDistanceLocale[token](count),
+  };
+
+  const seconds = parseInt(
+    formatDistanceStrict(new Date(baseDate), new Date(comparedDate), {
+      unit: 'second',
+    }),
+  );
+  const duration = intervalToDuration({ start: 0, end: seconds * 1000 });
+  const distance = formatDuration(duration, {
+    format: ['hours', 'minutes', 'seconds'],
+    locale: shortLocale,
+  });
+
+  return distance;
+};
