d3a45a4da4d44e1d10e587701bb119401f07c466
the1020 <yenn2322@gmail.com>
2022-05-24 18:29:57 +0900

    :sparkles: 활성 프로그램 변경 조건 수정 : app의 title이 바껴도 새 window로 간주

--- public/electron.js
+++ public/electron.js
@@ -29,24 +29,18 @@ app.whenReady().then(() => {
   }
 
   ipcMain.on('SELECTEDDAY_WINDOW', (event, payload) => {
-    //TODO 1. 오늘 목록만 저장할 변수? 관리하기 / 오늘 목록인지 확인할 변수도 필요함
-    //TODO 2. 이전/다음 날짜로 넘어갔을 때 선택한 날짜가 오늘이 아니면 리스트는 더 이상 업데이트 하지 않아야 함
-    //TODO 2-1. 추적중인 경우(start 버튼이 클릭된 경우)에 현재 추척 목록은 저장하되 리스트는 더 이상 업데이트 하지 않아야 하고
-    //TODO 2-2. 오늘 목록으로 넘어오면 따로 저장해둔 목록을 불러와야 함
-
     isSelectedDayToday = payload === format(new Date(), 'yyyy-MM-dd');
 
     const activeWindows = findTrackWindowByDate(payload);
     event.reply('REPLY_ACTIVE_WINDOW', { activeWindows });
   });
 
-  ipcMain.on('ACTIVE_WINDOW', async (event, payload) => {
-    let window = await activeWin({ screenRecordingPermission: true });
-
+  ipcMain.on('ACTIVE_WINDOW', async (event) => {
     //* store에는 "trackWindows"와 "currentActiveWin"이 저장되어 있음
     const trackWindows = store.get('trackWindows') || [];
     const currentActiveWin = store.get('currentActiveWin') || null;
 
+    const window = await activeWin({ screenRecordingPermission: true });
     const {
       id,
       title,
@@ -67,17 +61,15 @@ app.whenReady().then(() => {
     };
 
     //* 첫 할당이거나 활성 프로그램이 변했으면 재할당
-    if (!currentActiveWin || currentActiveWin.id !== processedWindow.id) {
-      //* 활성 프로그램이 바꼈으면 이전 프로그램 정보에 finishedDate 정보 추가해서 store에 저장
+    if (!currentActiveWin || !isSameWindow(currentActiveWin, processedWindow)) {
       if (currentActiveWin) {
-        //* 날짜 객체 생성해서 차이 구하기
         const { startDate } = currentActiveWin;
         const { startDate: finishedDate } = processedWindow;
         const distance = getDistanceDate(startDate, finishedDate);
 
-        //* 이전의 active-win 객체에 finishedDate와 distance 정보 업데이트
+        //* 활성 프로그램이 바꼈으면 이전의 active-win 객체에 finishedDate와 distance 정보 업데이트
         trackWindows.forEach((w) => {
-          if (w.id === currentActiveWin.id && w.startDate === currentActiveWin.startDate) {
+          if (isSameWindow(w, currentActiveWin) && w.startDate === currentActiveWin.startDate) {
             [w.finishedDate, w.distance] = [finishedDate, distance];
           }
         });
@@ -108,6 +100,14 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   app.quit();
 });
+
+const isSameWindow = (item1, item2) => {
+  if (item1 && item2 && item1.app === item2.app && item1.title === item2.title) {
+    return true;
+  }
+
+  return false;
+};
 
 const findTrackWindowByDate = (comparedDate) => {
   comparedDate = typeof comparedDate === 'string' ? new Date(comparedDate) : comparedDate;
